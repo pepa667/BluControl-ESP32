596f5c2942e1859a7e97913c066e5c9a1ddd1cbc
JPZV <38300410+JPZV@users.noreply.github.com>
2023-01-20 13:12:30 -0300

    Fixed Typo

--- blu-common/blu-components/triggers/blu-analog-triggers.c
+++ blu-common/blu-components/triggers/blu-analog-triggers.c
@@ -10,7 +10,7 @@ int right_trigger_center = 0;
 void blu_analog_triggers_init(void)
 {
     printf("BluControl: Starting analogs triggers.\n");
-    #if defined(CONFIG_BLUCONTROL_LEFT_TRIGGERS_ANALOG) || defined(CONFIG_BLUCONTROL_RIGHT_TRIGGERS_ANALOG)
+    #if defined(CONFIG_BLUCONTROL_LEFT_TRIGGER_ANALOG) || defined(CONFIG_BLUCONTROL_RIGHT_TRIGGER_ANALOG)
     blu_analog_init();
 
     adc_oneshot_chan_cfg_t adc_channel_config = {
@@ -18,14 +18,14 @@ void blu_analog_triggers_init(void)
         .atten = BLU_ADC_ATTENUATION,
     };
 
-    #ifdef CONFIG_BLUCONTROL_LEFT_TRIGGERS_ANALOG
+    #ifdef CONFIG_BLUCONTROL_LEFT_TRIGGER_ANALOG
     printf("BluControl: Starting left analog trigger.\n");
     ESP_ERROR_CHECK(adc_oneshot_config_channel(blu_adc1_handle, get_adc_channel_from_gpio(LEFT_TRIGGER_GPIO), &adc_channel_config));
     //Axis Calibration
     ESP_ERROR_CHECK(adc_oneshot_read(blu_adc1_handle, get_adc_channel_from_gpio(LEFT_TRIGGER_GPIO), &left_trigger_center));
     adc_cali_raw_to_voltage(blu_adc_cali_handle, left_trigger_center, &left_trigger_center);
     #endif
-    #ifdef CONFIG_BLUCONTROL_RIGHT_TRIGGERS_ANALOG
+    #ifdef CONFIG_BLUCONTROL_RIGHT_TRIGGER_ANALOG
     printf("BluControl: Starting right analog stick.\n");
     ESP_ERROR_CHECK(adc_oneshot_config_channel(blu_adc1_handle, get_adc_channel_from_gpio(RIGHT_TRIGGER_GPIO), &adc_channel_config));
     //Axis Calibration
